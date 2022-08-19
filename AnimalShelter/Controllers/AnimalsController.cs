083e7c2ec74c910b94e9f4374a44c307df27d938
Josephwjack <joseph.w.jack@gmail.com>
2022-08-19 11:41:54 -0700

    add animals controller complete with xml comments for swagger ui

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -4,36 +4,173 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using AnimalShelter.Models;
 
-namespace AnimalShelter.Controllers
+
+namespace Animals.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class AnimalsController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly AnimalShelterContext _db;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public AnimalsController(AnimalShelterContext db)
         {
-            _logger = logger;
+            _db = db;
         }
 
+        /// <summary>
+        /// Returns a list of all available animals.
+         /// </summary>
+
+        
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public ActionResult<IEnumerable<Animal>> Get(int age, string name, string species, string breed, string gender)
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var query = _db.Animals.AsQueryable();
+            if (name == null && species == null && gender == null && age == 0)
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                return _db.Animals.ToList();
+            }
+
+            if (name != null)
+            {
+                query = query.Where(entry => entry.Name == name);
+            }
+
+            if (species != null)
+            {
+                query = query.Where(entry => entry.Species == species);
+            }
+
+            if (gender != null)
+            {
+                query = query.Where(entry => entry.Gender == gender);
+            }
+
+            if (breed != null)
+            {
+                query = query.Where(entry => entry.Breed == breed);
+            }
+
+            if (age > 0)
+            {
+                query = query.Where(entry => entry.Age == age);
+            }
+
+            return query.ToList();
+        }
+
+        /// <summary>
+        /// Creates an animal listing.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        /// POST /animals
+        /// { 
+        ///    "AnimalId" = 1, 
+        ///    "Name" = "Rutabega", 
+        ///    "Gender" = "Female",
+        ///    "Species" = "Dog",
+        ///    "Breed" = "Pitty / Black lab mix",
+        ///    "Age" = 9,
+        ///    "Description" = "Loves to be a landshark."}
+        ///
+        /// </remarks>
+        /// <returns>A newly created Animal</returns>
+        
+        [HttpPost]
+        public async Task<ActionResult<Animal>> Post(Animal animal)
+        {
+        _db.Animals.Add(animal);
+        await _db.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
+        }
+
+        
+
+
+        /// <summary>
+        /// Searches for a specific animal by Id.
+        /// </summary>
+        /// <param name="id"></param>
+        // GET api/Animals/5
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Animal>> GetAnimal(int id)
+        {
+            var animal = await _db.Animals.FindAsync(id);
+
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            return animal;
+        }
+
+        
+        [HttpPut("{id}")] 
+        public async Task<IActionResult> Put(int id, [FromBody] Animal animal)
+        {
+        if (id != animal.AnimalId)
+        {
+            return BadRequest();
+        }
+
+        _db.Entry(animal).State = EntityState.Modified;
+
+        try 
+        {
+            await _db.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!AnimalExists(id))
+            {
+            return NotFound();
+            }
+            
+            
+            else
+            {
+            throw;
+            }
+        }
+
+        return NoContent();
+        }
+
+        /// <summary>
+        /// Deletes a specific animal.
+        /// </summary>
+        /// <param name="id"></param>
+        // DELETE api/animals/5
+        
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAnimal(int id)
+        {
+        var animal = await _db.Animals.FindAsync(id);
+        if (animal == null)
+        {
+            return NotFound();
+        }
+
+        _db.Animals.Remove(animal);
+        await _db.SaveChangesAsync();
+
+        return NoContent();
+        }
+
+        private bool AnimalExists(int id)    
+        {
+        return _db.Animals.Any(e => e.AnimalId == id);
         }
     }
 }
---
+++ AnimalShelter/Migrations/20220819182115_Initial.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    AnimalId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(30) CHARACTER SET utf8mb4", maxLength: 30, nullable: false),
+                    Gender = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Species = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Breed = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "varchar(300) CHARACTER SET utf8mb4", maxLength: 300, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.AnimalId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Breed", "Description", "Gender", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 9, "Pitty / Black lab mix", "Loves to be a landshark.", "Female", "Rutabega", "Dog" },
+                    { 2, 8, "Australian Mini Shepherd", "Would love an older dog for a brother.", "Male", "Jinx", "Dog" },
+                    { 3, 7, "Korat", "Beautiful soft green eyes. Korats are believed to be good luck.", "Male", "Thai", "Cat" },
+                    { 4, 4, "Bearded Dragon", "Loves silk worms and nuzzling in long hair or in sweatshirt hoods.", "Male", "Toothless", "Reptile" },
+                    { 5, 30, "Cockatoo", "Put on 'Lets go crazy' and be amazed. He'll dance to that song all day.", "Male", "Prince", "Bird" },
+                    { 6, 99, "Oak?", "He is Groot", "N/A", "Groot", "Tree?" },
+                    { 7, 9, "Maine Coon", "Sweet, fluffy and loves outdoors. Longhair that needs to be brushed to avoid dreads.", "Female", "Luna", "Cat" },
+                    { 8, 4, "Golden Retriever", "Loves swimming and being on boats.", "Female", "Penelope", "Dog" },
+                    { 9, 7, "Bambino", "Affectionate, friendly with kids and other pets, and very vocal.", "Male", "Rev", "Cat" },
+                    { 10, 11, "Axolotl", "Endangered species, new owner must have extensive knowledge of axolotls and pass multiple screenings.", "Female", "Mudkip", "Amphibian" },
+                    { 11, 2, "French bulldog", "Chews on fingers, shoes, literally everything.", "Male", "Gatsby", "Dog" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+        }
+    }
+}
