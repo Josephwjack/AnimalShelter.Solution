ea9d8db1d52f14acca31978218362dede56ddcc7
Josephwjack <joseph.w.jack@gmail.com>
2022-08-19 10:32:50 -0700

    add animal model with properties

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -6,7 +6,27 @@ namespace AnimalShelter.Models
     {
       public int AnimalId { get; set; }
 
+
+      [Required]
+      [StringLength(30)]
       public string Name { get; set; }
+
+      [Required]
+      public string Gender { get; set; }
+
+      [Required]
+      public string Species { get; set; }
+
+
+      public string Breed { get; set; }
+
+      [Required]
+      [Range(0, 100)]
+      public int Age { get; set; }
+
+
+      [StringLength(300, ErrorMessage = "Description must be less than 300 characters.")]
+      public string Description { get; set; }
 
 
 
